4be14e4c5cfe39d997e76b3127b728cb57796a6c
Daniel Cho <danielcho91@gmail.com>
2021-01-28 13:51:00 -0500

    car item component

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -4,35 +4,38 @@ import { View, Text, ImageBackground } from 'react-native';
 import styles from './styles';
 import StyledButton from '../StyledButton';
 
-const CariItem = () => {
+const CarItem = (props) => {
+  const { name, tagline, taglineCTA, image } = props;
+
   return (
     <View style={styles.carContainer}>
-      <ImageBackground
-        source={require('../../assets/images/ModelX.jpeg')}
-        style={styles.image}
-      />
+      <ImageBackground source={image} style={styles.image} />
 
       <View style={styles.titles}>
-        <Text style={styles.title}>Model S</Text>
-        <Text style={styles.subtitle}>Starting at $69,420</Text>
+        <Text style={styles.title}>{name}</Text>
+        <Text style={styles.subtitle}>
+          {tagline} <Text style={styles.subtitleCTA}>{taglineCTA}</Text>
+        </Text>
       </View>
 
-      <StyledButton
-        type='primary'
-        content='custom order'
-        onPress={() => {
-          console.warn('custom order was pressed');
-        }}
-      />
-      <StyledButton
-        type='secondary'
-        content='existing inventory'
-        onPress={() => {
-          console.warn('existing inventory was pressed');
-        }}
-      />
+      <View style={styles.buttonsContainer}>
+        <StyledButton
+          type='primary'
+          content='custom order'
+          onPress={() => {
+            console.warn('custom order was pressed');
+          }}
+        />
+        <StyledButton
+          type='secondary'
+          content='existing inventory'
+          onPress={() => {
+            console.warn('existing inventory was pressed');
+          }}
+        />
+      </View>
     </View>
   );
 };
 
-export default CariItem;
+export default CarItem;

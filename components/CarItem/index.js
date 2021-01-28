2eb56f62da800dfb18378be3e6da6c5845a162f3
Daniel Cho <danielcho91@gmail.com>
2021-01-28 13:38:29 -0500

    button component

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text, ImageBackground } from 'react-native';
 
 import styles from './styles';
+import StyledButton from '../StyledButton';
 
 const CariItem = () => {
   return (
@@ -10,10 +11,26 @@ const CariItem = () => {
         source={require('../../assets/images/ModelX.jpeg')}
         style={styles.image}
       />
+
       <View style={styles.titles}>
         <Text style={styles.title}>Model S</Text>
         <Text style={styles.subtitle}>Starting at $69,420</Text>
       </View>
+
+      <StyledButton
+        type='primary'
+        content='custom order'
+        onPress={() => {
+          console.warn('custom order was pressed');
+        }}
+      />
+      <StyledButton
+        type='secondary'
+        content='existing inventory'
+        onPress={() => {
+          console.warn('existing inventory was pressed');
+        }}
+      />
     </View>
   );
 };
